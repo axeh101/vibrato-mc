cc7bc776a7b8791f73bb360bbbe9b99baf08a25a
axeh101 <>
2021-04-21 18:20:29 +0200

    delta vibrato

--- src/engines/vibrato.hpp
+++ src/engines/vibrato.hpp
@@ -33,9 +33,9 @@ public:
 	virtual D _delta() {
 		D r = this->process_->rate();
 		D total = 0.;
-		D subtotal = 0.;
+
 		for (int i = 0; i < M; ++i) {
-			// First step
+			// First step: Compute the deltas up to the n-1
 			this->process_->resetState();
 			this->process_->initTangentState(1);
 			for (int i = 0; i < n - 1; ++i) {
@@ -46,20 +46,28 @@ public:
 			}
 			// End of first step
 
-			subtotal = 0.;
+			// Replication step
 			double t = this->process_->priceState().time;
 			D sigma = this->process_->vol(t);
 			D value = this->process_->priceState().value;
 			D tangent = this->process_->tangentState().value;
-
+			D fstHalf=0;
+			D scdHalf=0;
+			D rh = h * r;
+			D sigmah = sigma * sqrt(h);
+			D subtotal = 0.;
+			D mun = value * (1 + rh);
+			D sigman = value * sigmah ;
+			D dmun =  tangent * (1 + rh);
+			D dsigman =  2 * tangent * sigmah * value * sigmah;
 			for (int j = 1; j <= Mz; j++) {
 				D Z = normal();
-				D rh = h * r;
-				D sigmah = sigma * sqrt(h);
-				D V = this->option_.payoff(
-						value * (1 + rh) + value * sigmah * Z);
-				subtotal += tangent * ((1 + rh) * V + V * (Z * Z - 1)) / sigmah;
+				D V = this->option_.payoff(mun + sigman * sqrt(h) * Z );
+				fstHalf += V * Z / sigman;
+				scdHalf += V * (Z * Z - 1) / (sigman * sigman * value * value);
 			}
+			subtotal = dmun * fstHalf / sqrt(h) + dsigman * scdHalf / (2*h);
+			// End of Replication step
 			total += subtotal / Mz;
 		}
 		return exp(-this->process_->rate() * T) * total / n;
@@ -69,6 +77,45 @@ public:
 private:
 	NormalDistribution<D> normal = NormalDistribution<D>(0, 1);
 	double T;
+//
+//	void genGamma() {
+//		this->process_.resetState();
+//		this->process_->initTangentState(1);
+//		this->process_->initTangentState(2);
+//		for (int i = 0; i < n - 1; ++i) {
+//			D Wh = h * normal();
+//			this->process_->movePriceEuler(h, Wh);
+//			this->process_->moveTangentEuler(h, Wh);
+//			this->process_->moveTangent2Euler(h, Wh);
+//		}
+//	}
+//
+//	void genVega(double t) {
+//		this->process_->resetState();
+//		this->process_->initTangentState(0);
+//		for (int i = 0; i < n - 1; ++i) {
+//			D Wh = h * normal();
+//			tangentState = this->process_->moveTangentEuler(h, Wh);
+//			tangentState.value += priceState.value * Wh;
+//			priceState = this->process_->movePriceEuler(h, Wh);
+//		}
+//	}
+//
+//	void genVanna(double t) {
+//		this->process_.resetState();
+//		State<D> priceState = this->process_->initialState;
+//		State<D> tangentState = { priceState.time, 1 };
+//		State<D> tangentState2 = { priceState.time, 0.0001 };
+//		double time = priceState.time;
+//		for (int i = 0; i < n - 1; ++i) {
+//			D Wh = h * normal();
+//			tangentState2 = this->process_->moveTangent2Euler(h, Wh);
+//			tangentState2.value += 2 * tangentState.value * Wh;
+//			tangentState = this->process_.moveTangentEuler(h, Wh);
+//			tangentState2.value += priceState.value * Wh;
+//			priceState = this->process_.movePriceEuler(h, Wh);
+//		}
+//	}
 
 };
 
