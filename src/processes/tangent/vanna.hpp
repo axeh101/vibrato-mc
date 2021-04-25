b30865e2d4449635948544904fdf5bc699a31aa9
axeh101 <>
2021-04-25 06:38:05 +0200

    add vanna tangent

--- src/processes/tangent/vanna.hpp
+++ src/processes/tangent/vanna.hpp
@@ -1,6 +1,43 @@
 #ifndef VANNA_HPP
 #define VANNA_HPP
 
+#include <typeinfo>
+
+#include "../process.hpp"
+
+// TODO: Check the gamma tangent and correct the generation process
+
+template<typename T>
+class VannaTangent : public TangentProcess<T> {
+
+private:
+    TangentProcess<T> *tangent1_;
+    TangentProcess<T> *tangent2_;
+public:
+    VannaTangent(State<T> initialState, TangentProcess<T> *tangent1, TangentProcess<T> *tangent2) :
+            TangentProcess<T>(initialState, "Vanna Tangent process", tangent1->parent_),
+            tangent1_(tangent1), tangent2_(tangent2) {}
+
+    virtual~VannaTangent() = default;
+
+    virtual T drift() const override {
+        return (
+                this->parent_->diffDriftX() * this->priceState_.value +
+                this->parent_->diffDriftX2() * tangent1_->priceState().value *
+                tangent2_->priceState().value
+        );
+    }
+
+    virtual T diffusion() const override {
+        return (
+                this->parent_->diffDiffusionSigmaX() * tangent1_->priceState().value +
+                this->parent_->diffDiffusionX2() * tangent1_->priceState().value *
+                tangent2_->priceState().value +
+                this->parent_->diffDiffusionX() * this->priceState_.value
+        );
+    }
+
+};
 
 
 #endif  // VANNA_HPP
