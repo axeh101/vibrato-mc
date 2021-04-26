050b3f7cb16da761ff61b776f9e48ecdfeed9c60
axeh101 <>
2021-04-26 07:31:19 +0200

    remove unused classes

--- src/processes/helper.hpp
+++ src/processes/helper.hpp
@@ -16,18 +16,6 @@ struct State {
 };
 
 template<typename T>
-struct StateWithTangent {
-    double time;
-    T value;
-    T tangent;
-
-    friend std::ostream &operator<<(std::ostream &o, StateWithTangent<T> const &s) {
-        return o << s.time << ";" << s.value << ";" << s.tangent;
-    }
-};
-
-
-template<typename T>
 struct Path : protected std::vector<State<T>> {
     using vec = std::vector<State<T>>;
     using vec::vec;
@@ -37,22 +25,6 @@ struct Path : protected std::vector<State<T>> {
     using vec::size;
 
     friend std::ostream &operator<<(std::ostream &o, Path<T> const &p) {
-        for (auto const &st : p)
-            o << st << std::endl;
-        return o << std::endl;
-    }
-};
-
-template<typename T>
-struct PathWithTangent : protected std::vector<StateWithTangent<T>> {
-    using vec = std::vector<State<T>>;
-    using vec::vec;
-    using vec::operator[];
-    using vec::begin;
-    using vec::end;
-    using vec::size;
-
-    friend std::ostream &operator<<(std::ostream &o, PathWithTangent<T> const &p) {
         for (auto const &st : p)
             o << st << std::endl;
         return o << std::endl;
