2f46876386a630888406bc322a4a1be356c727ad
axeh101 <>
2021-04-26 06:33:20 +0200

    remove unused vars

--- src/engines/analytic_digital_bs.hpp
+++ src/engines/analytic_digital_bs.hpp
@@ -18,9 +18,7 @@ private:
     D ract;
     D d1;
     D d2;
-    D nd1;
     D nd2;
-    D pd1;
     D pd2;
 
     void setParams(void) {
@@ -32,10 +30,21 @@ private:
         ract = exp(-r * T);
         d1 = (log(S / K) + (r + 0.5 * sigma * sigma) * T) / (sigma * sqrt(T));
         d2 = d1 - sigma * sqrt(T);
-        nd1 = normal.cdf(d1);
         nd2 = normal.cdf(d2);
-        pd1 = normal.pdf(d1);
         pd2 = normal.pdf(d2);
+    }
+
+
+    virtual D _premium() {
+        if (this->option_->type() == OptionType::Call) {
+            return ract * nd2;
+        } else {
+            return ract * (1 - nd2);
+        }
+    }
+
+    virtual D _delta() const {
+        return this->option_->type() * ract * pd2 / (sigma * sqrt(T) * S);
     }
 
     virtual D _rho() const {
@@ -52,9 +61,6 @@ private:
         return -d1 * pd2 * ract / sigma;
     }
 
-    virtual D _delta() const {
-        return this->option_->type() * ract * pd2 / (sigma * sqrt(T) * S);
-    }
 
     virtual D _gamma() const {
         return _vega() / (sigma * S * S * T);
@@ -78,13 +84,6 @@ private:
         return ract * pd2 * (d1 * d1 * d2 - d1 - d2) / (sigma * sigma);
     }
 
-    virtual D _premium() {
-        if (this->option_->type() == OptionType::Call) {
-            return ract * nd2;
-        } else {
-            return ract * (1 - nd2);
-        }
-    }
 
 public:
     AnalyticalDigitalBS(Option<D> *option, BlackScholesProcess<D> *process)
