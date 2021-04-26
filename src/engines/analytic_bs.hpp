4edad1350b98d084ad5ac02a87390ca2c8e9c724
axeh101 <>
2021-04-26 06:33:16 +0200

    remove unused vars

--- src/engines/analytic_bs.hpp
+++ src/engines/analytic_bs.hpp
@@ -21,7 +21,6 @@ private:
     D nd1;
     D nd2;
     D pd1;
-    D pd2;
 
     void setParams(void) {
         r = this->process_->rate();
@@ -35,7 +34,6 @@ private:
         nd1 = normal.cdf(d1);
         nd2 = normal.cdf(d2);
         pd1 = normal.pdf(d1);
-        pd2 = normal.pdf(d2);
     }
 
     virtual D _rho() const {
@@ -65,19 +63,19 @@ private:
 
     virtual D _theta() const {
         if (this->option_->type() == OptionType::Call) {
-            return S * pd1 * sigma / (2 * sqrt(T)) + r * K * ract * nd2;
+            return -S * pd1 * sigma / (2 * sqrt(T)) - r * K * ract * nd2;
         } else {
-            return S * pd1 * sigma / (2 * sqrt(T)) - r * K * ract * (1 - nd2);
+            return -S * pd1 * sigma / (2 * sqrt(T)) + r * K * ract * (1 - nd2);
         }
     }
 
     virtual D _vanna() const {
-        return this->vega_ * (1 - d1 / (sigma * sqrt(T))) / S;
+        return -pd1 * d2 / sigma;
 
     }
 
     virtual D _volga() const {
-        return this->vega_ * d1 * d2 / sigma;
+        return S * sqrt(T) * pd1 * d1 * d2 / (sigma);
     }
 
     virtual D _premium() {
