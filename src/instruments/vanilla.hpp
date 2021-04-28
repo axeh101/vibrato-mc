69ed67e13d5f4879f89d95a4bacf5591beb67035
axeh101 <>
2021-04-29 01:19:02 +0200

    refactor

--- src/instruments/vanilla.hpp
+++ src/instruments/vanilla.hpp
@@ -13,11 +13,12 @@ public:
             Option<T>(maturity, strike, type, "Vanilla Option") {};
 
     T payoff(T price) const override {
-        return max(this->type_ * (price - this->strike_), 0.0);
+        return max(this->type_ * (price - this->strike_), 0.);
     };
 
     dual payoff(dual price) const override {
-        return ((price - this->strike_) > (dual)0.) ? (price - this->strike_) : (dual) 0.;
+        dual value = (dual)this->type_ * (price - this->strike_);
+        return ( value> (dual)0.) ? value : (dual) 0.;
     }
 };
 
