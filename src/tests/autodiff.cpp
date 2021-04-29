90c44504a79f0334a2fd89a96cd8e7126846b604
axeh101 <>
2021-04-29 17:50:36 +0200

    new autodiff test

---
+++ src/tests/autodiff.cpp
@@ -0,0 +1,26 @@
+//
+// Created by abenmoussa on 4/29/21.
+//
+#include <iostream>
+#include <autodiff/forward.hpp>
+
+using namespace autodiff;
+
+
+dual f(dual x){
+    dual val = x-5;
+    return val>0?val: 0;
+}
+
+int main(){
+    dual x = 0;
+    dual y = 0;
+    for (int i=0; i< 10; i++)
+    {
+        y +=1;
+        std::cout << i << ": y = " << y << std::endl;
+        std::cout << i << ": f(y) = " << f(y) << std::endl;
+        std::cout << i << ": df/dy = " << derivative(f, wrt(y), at(y)) << std::endl;
+    }
+    return 0;
+}
