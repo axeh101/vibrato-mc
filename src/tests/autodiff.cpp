e3d398b7227358e35c0dc75de5dcf934394facb3
axeh101 <>
2021-04-30 01:12:02 +0200

    change autodiff

--- src/tests/autodiff.cpp
+++ src/tests/autodiff.cpp
@@ -3,9 +3,7 @@
 //
 #include <iostream>
 #include <autodiff/forward.hpp>
-
 using namespace autodiff;
-
 
 dual f(dual x) {
     dual val = x - 5;
@@ -16,9 +14,8 @@ int main() {
     dual y = 0;
     for (int i = 0; i < 10; i++) {
         y += 1;
-        std::cout << i << ": y = " << y << std::endl;
-//        std::cout << i << ": f(y) = " << f(y) << std::endl;
-        std::cout << i << ": df/dy = " << derivative(f, wrt(y), at(y)) << std::endl;
+        std::cout << i << ": f(y) = " << f(y) << std::endl; // Renvoie la valeur de la fonction au point y
+        std::cout << i << ": df/dy = " << derivative(f, wrt(y), at(y)) << std::endl; // Renvoie df/dy (y)
     }
     return 0;
 }
