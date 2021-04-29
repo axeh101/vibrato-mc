f26c6cf6b9fccbbaf974cd36034e527db1e40a6d
axeh101 <>
2021-04-29 13:03:17 +0200

    update ad

--- src/engines/vibratoad.hpp
+++ src/engines/vibratoad.hpp
@@ -23,6 +23,11 @@ public:
 
     D gamma() {
         D total = 0;
+        // Fonction permettant de calculer le delta dont on va calculer la dérivée par AD.
+        // On utilise une expression lambda car la librairie ne supporte pas les méthodes
+        // de classe
+        auto f =[&](auto x) {return this->_f_delta(x);};
+
         for (int i = 0; i < M; ++i) {
             this->process_->resetState();
             deltaTangentProcess.resetState();
@@ -32,13 +37,15 @@ public:
                 this->process_->movePriceEuler(h, Z);
             }
             dual X = this->process_->priceState().value;
-            total += derivative([&](auto x) { return this->_f_delta_wrt_price(x); }, wrt(X), at(X));
+            total += derivative(f, wrt(X), at(X));
         }
-        return exp(-this->process_->rate() * 2 * T) * total / M;
+        return exp(-this->process_->rate() * T) * total / (M*Mz);
     }
 
     D vanna() override {
         D total = 0;
+        // Fonction permettant de calculer le vega dont on va calculer la dérivée pa rapport a X par AD.
+        auto f =[&](auto x, auto y) { return this->_f_vega(x, y); };
         for (int i = 0; i < M; ++i) {
             this->process_->resetState();
             vegaTangentProcess.resetState();
@@ -47,14 +54,17 @@ public:
                 vegaTangentProcess.movePriceEuler(h, Z);
                 this->process_->movePriceEuler(h, Z);
             }
+            dual sigma = this->process_->vol();
             dual X = this->process_->priceState().value;
-            total += derivative([&](auto x) { return this->_f_vega_wrt_x(x); }, wrt(X), at(X));
+            total += derivative(f, wrt(X), at(X, sigma));
         }
-        return exp(-this->process_->rate() * T) * total / M;
+        return exp(-this->process_->rate() * T) * total / (M*Mz);
     }
 
     D volga() override {
         D total = 0;
+        // Fonction permettant de calculer le vega dont on va calculer la dérivée pa rapport a sigma par AD.
+        auto f =[&](auto x, auto y) { return this->_f_vega(x, y); };
         for (int i = 0; i < M; ++i) {
             this->process_->resetState();
             vegaTangentProcess.resetState();
@@ -63,11 +73,12 @@ public:
                 vegaTangentProcess.movePriceEuler(h, Z);
                 this->process_->movePriceEuler(h, Z);
             }
-            dual X = this->process_->vol();
-            total += derivative([&](auto x) { return this->_f_vega_wrt_sigma(x); }, wrt(X), at(X));
+            dual sigma = this->process_->vol();
+            dual X = this->process_->priceState().value;
+            total += derivative(f, wrt(sigma), at(X, sigma));
         }
 
-        return exp(-this->process_->rate() * T) * total / M;
+        return exp(-this->process_->rate() * T) * total / (M*Mz);
     }
 
     virtual ~VibratoAD() = default;
@@ -79,14 +90,14 @@ private:
     NormalDistribution<D> normal = NormalDistribution<D>(0, 1);
     D T;
 
-    dual _f_delta_wrt_price(dual X) {
+    dual _f_delta(dual X) {
         D Z;
         dual espmu = 0;
         dual espsigma = 0;
         dual mun = X * (1 + this->process_->rate() * h);
         dual sigman = X * this->process_->vol() * sqrt(h);
-        D dmun = deltaTangentProcess.dmun(h);
-        D dsigman = deltaTangentProcess.dsigman(h);
+        dual dmun = deltaTangentProcess.dmun(h);
+        dual dsigman = deltaTangentProcess.dsigman(h);
         if (antithetic) {
             dual payoffPlus;
             dual payoffMinus;
@@ -108,17 +119,17 @@ private:
                 espsigma += (Z * Z - 1) * p / (2 * sigman * sigman);
             }
         }
-        return (dmun * espmu + dsigman * espsigma) / Mz;
+        return (dmun * espmu + dsigman * espsigma);
     }
 
-    dual _f_vega_wrt_x(dual X) {
-        D Z;
+    dual _f_vega(dual X, dual sigma) {
+        dual Z;
         dual espmu = 0;
         dual espsigma = 0;
+        dual sigman = X * sigma * sqrt(h);
+        dual dmun = vegaTangentProcess.dmun(h);
         dual mun = X * (1 + this->process_->rate() * h);
-        dual sigman = X * this->process_->vol() * sqrt(h);
-        D dmun = vegaTangentProcess.dmun(h);
-        dual dsigman = X + this->process_->vol() * vegaTangentProcess.priceState().value * sqrt(h);
+        dual dsigman = X + sigma * vegaTangentProcess.priceState().value * sqrt(h);
         if (antithetic) {
             dual payoffPlus;
             dual payoffMinus;
@@ -140,40 +151,7 @@ private:
                 espsigma += (Z * Z - 1) * p / (2 * sigman * sigman);
             }
         }
-        return (dmun * espmu + dsigman * espsigma) / Mz;
-    }
-
-    dual _f_vega_wrt_sigma(dual sigma) {
-        D Z;
-        dual espmu = 0;
-        dual espsigma = 0;
-        D mun = vegaTangentProcess.mun(h);
-        dual sigman = this->process_->priceState().value * sigma * sqrt(h);
-        D dmun = vegaTangentProcess.dmun(h);
-        dual dsigman =
-                (vegaTangentProcess.priceState().value + sigma * vegaTangentProcess.priceState().value) * sqrt(h);
-        if (antithetic) {
-            dual payoffPlus;
-            dual payoffMinus;
-            dual payoffMu;
-            for (int j = 0; j < Mz; j++) {
-                Z = normal();
-                payoffPlus = this->option_->payoff(mun + sigman * Z);
-                payoffMinus = this->option_->payoff(mun - sigman * Z);
-                payoffMu = this->option_->payoff(mun);
-                espmu += Z * (payoffPlus - payoffMinus) / (2 * sigman);
-                espsigma += (Z * Z - 1) * (payoffPlus - 2 * payoffMu + payoffMinus) / (2 * sigman);
-            }
-        } else {
-            dual p;
-            for (int j = 0; j < Mz; j++) {
-                Z = normal();
-                p = this->option_->payoff(mun + sigman * Z);
-                espmu += Z * p / (sigman);
-                espsigma += 0.5 * (Z * Z - 1) * p / (sigman * sigman);
-            }
-        }
-        return (dmun * espmu + dsigman * espsigma) / Mz;
+        return (dmun * espmu + dsigman * espsigma);
     }
 };
 
