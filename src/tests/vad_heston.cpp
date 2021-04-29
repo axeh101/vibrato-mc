a7ee60cac601634d9d541d709fd6260f59ac2ded
axeh101 <>
2021-04-30 01:18:15 +0200

    update heston

--- src/tests/vad_heston.cpp
+++ src/tests/vad_heston.cpp
@@ -35,28 +35,24 @@ int main() {
     BlackScholesProcess<dual> bs(initialState, rate, vol);
 
     // Pricing engines definition
-    auto ve = VibratoAD<dual>(&o, &bs, n, M, Mz);
-    auto be = AnalyticalDigitalBS<dual>(&o, &bs);
+    auto ve = VibratoAD<dual>(&o, &heston, n, M, Mz);
+    auto be = AnalyticalVanillaBS<dual>(&o, &bs);
 
     size_t vecSize = 100;
     int step = 1;
 
     // Black Scholes
+
     Helper<dual>::generateGreek(&o, &bs, [&](void) -> dual { return be.gamma(); }, price, vecSize, step,
                                 "_analytic_h_gamma");
     Helper<dual>::generateGreek(&o, &bs, [&](void) -> dual { return be.vanna(); }, price, vecSize, step,
                                 "_analytic_h_vanna");
-    Helper<dual>::generateGreek(&o, &bs, [&](void) -> dual { return be.volga(); }, price, vecSize, step,
-                                "_analytic_h_volga");
-
 
     ve.antithetic = false;
     Helper<dual>::generateGreek(&o, &heston, [&](void) -> dual { return ve.gamma(); }, price, vecSize, step,
                                 "_vibrato_heston_ad_gamma");
     Helper<dual>::generateGreek(&o, &heston, [&](void) -> dual { return ve.vanna(); }, price, vecSize, step,
                                 "_vibrato_heston_ad_vanna");
-    Helper<dual>::generateGreek(&o, &heston, [&](void) -> dual { return ve.volga(); }, price, vecSize, step,
-                                "_vibrato_heston_ad_volga");
 
     // Antithetic outputs
     ve.antithetic = true;
@@ -64,8 +60,6 @@ int main() {
                                 "_vibrato_heston_ad_gamma_anti");
     Helper<dual>::generateGreek(&o, &heston, [&](void) -> dual { return ve.vanna(); }, price, vecSize, step,
                                 "_vibrato_heston_ad_vanna_anti");
-    Helper<dual>::generateGreek(&o, &heston, [&](void) -> dual { return ve.volga(); }, price, vecSize, step,
-                                "_vibrato_heston_ad_volga_anti");
 
 
     std::cout << "***** Vibrato  AD for vanilla (Heston) terminated!" << std::endl;
