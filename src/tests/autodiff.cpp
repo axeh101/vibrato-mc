47925a3573e3932a4e1cd9ec834841e466138d09
axeh101 <>
2021-04-29 20:17:33 +0200

    update autodiff test

--- src/tests/autodiff.cpp
+++ src/tests/autodiff.cpp
@@ -13,12 +13,11 @@ dual f(dual x) {
 }
 
 int main() {
-    dual x = 0;
     dual y = 0;
     for (int i = 0; i < 10; i++) {
         y += 1;
         std::cout << i << ": y = " << y << std::endl;
-        std::cout << i << ": f(y) = " << f(y) << std::endl;
+//        std::cout << i << ": f(y) = " << f(y) << std::endl;
         std::cout << i << ": df/dy = " << derivative(f, wrt(y), at(y)) << std::endl;
     }
     return 0;
